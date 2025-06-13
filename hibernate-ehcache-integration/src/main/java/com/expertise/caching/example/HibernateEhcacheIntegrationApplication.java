bdf1c35903c1540d6df371d08e368c6f8270f1f4
vishal-chothe-patil <vishalchothe134@gmail.com>
2025-06-13 22:48:52 +0530

    Initial commit: hibernate-ehcache-integration project created

---
+++ hibernate-ehcache-integration/src/main/java/com/expertise/caching/example/HibernateEhcacheIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.expertise.caching.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateEhcacheIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateEhcacheIntegrationApplication.class, args);
+	}
+
+}
